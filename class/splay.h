deeb462e240ccae88fcf80ba19a525c0ab252499
Ashot <mnatsakanyan.ashot@gmail.com>
2014-11-20 20:03:01 +0400

    add copy info

--- class/splay.h
+++ class/splay.h
@@ -16,9 +16,9 @@ public:
     void insert(T value);
     bool remove(T value);
 
-    void pre_order(void (*f)(T &value)) const;
-    void in_order(void (*f)(T &value)) const;
-    void post_order(void (*f)(T &value)) const;
+    void pre_order(void (*f)(T &value)) const; // copy from bst tree
+    void in_order(void (*f)(T &value)) const; // copy from bst tree
+    void post_order(void (*f)(T &value)) const; // copy from bst tree
 
     Splay<T> *operator+(const Splay<T> &tree);
     Splay<T> *operator-(const Splay<T> &tree);
@@ -27,16 +27,15 @@ protected:
     struct Node;
 
     void splay_(Node *node);
+    void rotate_(Node *x);
 
-    T *search_(Node *node, T &value);
-    void insert_(Node *&node, T &value);
+    T *search_(Node *node, T &value) const; // copy from bst tree
+    void insert_(Node *&node, T &value, Node *parent = NULL);
     bool remove_(Node *&node, T &value, Node *parent = NULL);
 
-    void pre_order_(Node *node, void (*f)(T &value)) const;
-    void in_order_(Node *node, void (*f)(T &value)) const;
-    void post_order_(Node *node, void (*f)(T &value)) const;
-
-    void rotate_(Node *x);
+    void pre_order_(Node *node, void (*f)(T &value)) const; // copy from bst tree
+    void in_order_(Node *node, void (*f)(T &value)) const; // copy from bst tree
+    void post_order_(Node *node, void (*f)(T &value)) const; // copy from bst tree
 
     void merge_(Node *&node, Node *node_1, Node *node_2);
     void split_(Node *&node, Node *node_1, Node *node_2);
